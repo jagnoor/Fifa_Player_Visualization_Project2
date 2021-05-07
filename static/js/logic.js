d9c3f8ad6995ca2a549e6b005cf089c2edbc01be
chrispope12391 <popex107@umn.edu>
2021-05-06 19:02:48 -0500

    react graphs

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,55 +3,141 @@ console.log("logic.js loaded");
 // index.html graph ///////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////////
 
-function reactGraphs() {
+    d3.json("/fifadata").then( earnData => {
+
+      var highEarn = earnData.filter((a => a.wage_eur > 295000));
+      highEarn.sort((a,b) => (a.wage_eur < b.wage_eur) ? 1 : -1);
+  
+      // console.log(highEarn);
+  
+      highName = highEarn.map(function(d) {return d.short_name;});
+      earnValue = highEarn.map(function(d) {return d.wage_eur;});
 
     var trace1 = {
-        x: ["beer", "wine", "martini", "margarita",
-          "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-        y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-        type: "bar"
-      };
-    
-      var trace2 = {
-        x: ["beer", "wine", "martini", "margarita",
-          "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-        y: [2.7, 7.1, 99.9, 108.7, 7.2, 6.1, 56.0, 44.6],
-        type: "new bar"
-      };
-      
-      var trace3 = {
-        x: ["beer", "wine", "martini", "margarita",
-          "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-        y: [122.7, 0.1, 9.9, 58.7, 55.2, 6.1, 56.0, 44.6],
-        type: "bar"
-      };
-      
-    
-      var data = [trace1];
-    
-      var dataNew = [trace2];
-    
-      var dataNewNew = [trace3];
-      
-      var layout = {
-        title: "'Bar' Chart"
-      };
-      
-      var layoutNew = {
-        title: "Scatter Chart"
-      };
-    
-      Plotly.newPlot("new-plot", data, layout);
-    
-      Plotly.newPlot("plot-bar", dataNew, layoutNew);
-    
-      Plotly.newPlot("plot-b", dataNewNew, layout);
-    
-      Plotly.newPlot("plot-c", dataNewNew, layout);
-    
-    }
-    
-    reactGraphs ();
+      x: highName,
+      y: earnValue,
+      type: "bar"
+    };
+
+    var earnData = [trace1];
+
+    var earnLayout = {
+      title: "Top 11 Fifa Earners"
+    };
+
+    Plotly.newPlot("high-earn", earnData, earnLayout);
+
+  });
+
+  d3.json("/fifadata").then( country => {
+
+    topCountry = country.map(function(d) {return d.nationality;});
+
+    var counts = {};
+    for (var i = 0; i < topCountry.length; i++) {
+        counts[topCountry[i]] = 1 + (counts[topCountry[i]] || 0);
+}
+
+    var sortCount = Object.keys(counts).map(function (key) {
+      return [key, counts[key]];
+    });
+
+    // Sort the array based on the second element
+    sortCount.sort(function (first, second) {
+      return second[1] - first[1];
+    });
+
+    topCount = sortCount.slice(0,5);
+
+
+    // console.log(topCount);
+
+    var trace2 = {
+      x: [1497,1055,922,881,796].reverse(),
+      y: ["England", "Germany", "Spain", "France", "Argentina"],
+      type: "bar",
+      orientation: "h"
+    };
+
+    var countData = [trace2];
+
+    var countLayout = {
+      title: "Top Fifa Producing Countries"
+    };
+
+    Plotly.newPlot("country", countData, countLayout);
+
+});
+
+d3.json("/fifadata").then( age => {
+
+  age.forEach(function(d) {
+    d.age = +d.age;
+  });
+
+  playerAge = age.map(function(d) {return d.age;});
+
+  var ageCounts = {};
+  for (var i = 0; i < playerAge.length; i++) {
+      ageCounts[playerAge[i]] = 1 + (ageCounts[playerAge[i]] || 0);
+}
+
+  var sortAge = Object.keys(ageCounts).map(function (key) {
+    return [key, ageCounts[key]];
+  });
+
+  // console.log(sortAge);
+
+  var trace3 = {
+    x: [16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42],
+    y: [12,197,570,911,1124,1251,1285,1205,1186,1188,1142,1213,961,895,776,718,505,391,246,249,84,54,24,11,7,2,1],
+    type: "bar"
+  };
+
+  var ageData = [trace3];
+
+  var ageLayout = {
+    title: "Fifa Athlete Count by Age"
+  };
+
+  Plotly.newPlot("age", ageData, ageLayout);
+
+});
+  
+d3.json("/fifadata").then( club => {
+
+  overSort = club.sort((a,b) => (a.overall <= b.overall) ? 1 : -1);
+
+  overFilter = overSort.slice(0,30);
+
+  clubFilter = overFilter.map(function(d) {return d.club;});
+
+  var clubCounts = {};
+  for (var i = 0; i < clubFilter.length; i++) {
+      clubCounts[clubFilter[i]] = 1 + (clubCounts[clubFilter[i]] || 0);
+}
+  console.log(clubCounts);
+
+  var trace4 = {
+    labels: ["Arsenal", "Dortmund", "Chelse", "FC Barcelona", "FC Bayern", "Inter", "Juventus", "Liverpool", "Man City", "Man U", "Napoli", "Paris SG", "Real Madrid", "Tottenham"],
+    values: [1, 1, 1, 5, 1, 1, 3, 2, 4, 1, 1, 3, 4, 2],
+    type: "pie"
+  };
+
+  var clubData = [trace4];
+
+  var clubLayout = {
+    title: "Clubs with the Most Players in the Top 30"
+  };
+
+  Plotly.newPlot("club", clubData, clubLayout);
+
+
+
+  // console.log(overFilter);
+});
+  
+
 
   //   require.config({
   //     paths: {
@@ -103,7 +189,7 @@ function reactGraphs() {
       data["children"].push(dict); 
     }
 
-    console.log(data); 
+    // console.log(data); 
 
     var argentina = []
     for (var i = 0; i < argentina_filter.length; i++) {
@@ -143,7 +229,7 @@ function reactGraphs() {
 
 
 
-    console.log(argentina);
+    // console.log(argentina);
 
 
 
